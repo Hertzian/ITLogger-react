b48f50bfdcf25e16250d11f78783adac37a2662f
Lalitus <tallitosan@gmail.com>
2020-07-12 22:00:18 -0500

    AddLogModal, AddBtn components

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import LogItem from './LogItem';
-import Preloader from '../layout/Preloader';
+import {Preloader} from '../layout/Preloader';
 
 export const Logs = () => {
     const [logs, setLogs] = useState([]);
@@ -22,7 +22,7 @@ export const Logs = () => {
     }
     
     if(loading){
-        return <h4>Loading...</h4>
+        return <Preloader />
     }
     return (
         <ul className='collection with-header'>
