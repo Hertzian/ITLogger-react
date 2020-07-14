77b11ace3c224ef920d75a7dde6479f8b6c902cf
Lalitus <tallitosan@gmail.com>
2020-07-14 05:25:54 -0500

    SearchBar, EditLogModal functionality, updated their respective reducers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, {Fragment, useEffect} from 'react';
-import {SearchBar} from './components/layout/SearchBar';
+import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import {AddBtn} from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
-import {EditLogModal} from './components/logs/EditLogModal';
+import EditLogModal from './components/logs/EditLogModal';
 import {AddTechModal} from './components/techs/AddTechModal';
 import {TechListModal} from './components/techs/TechListModal';
 import {Provider} from 'react-redux';
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, {useRef} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {searchLogs} from '../../actions/logActions';
 
-export const SearchBar = () => {
+const SearchBar = ({searchLogs}) => {
+    const text = useRef('');
+
+    const onChange = e => {
+        searchLogs(text.current.value);
+    }
+
     return (
         <nav style={{marginBottom: '30px'}} className='blue'>
             <div className="nav-wrapper">
                 <form>
                     <div className="input-field">
-                    <input id="search" type="search" />
+                    <input
+                        id="search"
+                        type="search"
+                        placeholder='Search logs...'
+                        ref={text}
+                        onChange={onChange}
+                    />
                     <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
                     <i className="material-icons">close</i>
                     </div>
@@ -14,4 +29,10 @@ export const SearchBar = () => {
             </div>
         </nav>
     )
+};
+
+SearchBar.proptTypes = {
+    searchLogs: PropTypes.func.isRequired,
 }
+
+export default connect(null, {searchLogs})(SearchBar)
